4a3abb46414fca341b4f389bc6d99a21542c3bb7
Daniele Rosario <realskullbock@gmail.com>
2019-01-15 15:19:06 +0100

    Add Password Checks from user Component params
    This PR adds the same checks that Joomla\CMS\Form\Rule\PasswordRule implements for the normal com_users component.

--- component/frontend/Model/Subscribe/Validation/Password.php
+++ component/frontend/Model/Subscribe/Validation/Password.php
@@ -11,6 +11,7 @@ defined('_JEXEC') or die;
 
 use Akeeba\Subscriptions\Site\Model\Subscribe;
 
+
 /**
  * Validates the password
  *
@@ -41,6 +42,87 @@ class Password extends Base
 
 		// If the two password fields do not match the password validation fails
 		if ($this->state->password != $this->state->password2)
+		{
+			return false;
+		}
+		
+		// If we have parameters from com_users, use those instead.
+		// Some of these may be empty for legacy reasons.
+		$params = ComponentHelper::getParams('com_users');
+
+		if (empty($params))
+		{
+			return true;
+		}
+		
+		$minimumLengthp    = $params->get('minimum_length');
+		$minimumIntegersp  = $params->get('minimum_integers');
+		$minimumSymbolsp   = $params->get('minimum_symbols');
+		$minimumUppercasep = $params->get('minimum_uppercase');
+		$meterp            = $params->get('meter');
+		$thresholdp        = $params->get('threshold');
+
+		empty($minimumLengthp) ? : $minimumLength = (int) $minimumLengthp;
+		empty($minimumIntegersp) ? : $minimumIntegers = (int) $minimumIntegersp;
+		empty($minimumSymbolsp) ? : $minimumSymbols = (int) $minimumSymbolsp;
+		empty($minimumUppercasep) ? : $minimumUppercase = (int) $minimumUppercasep;
+		empty($meterp) ? : $meter = $meterp;
+		empty($thresholdp) ? : $threshold = $thresholdp;
+
+		$valueLength = strlen($value);
+
+		// We set a maximum length to prevent abuse since it is unfiltered.
+		if ($valueLength > 4096)
+		{
+			return false;
+		}
+
+		// We don't allow white space inside passwords
+		$valueTrim = trim($value);
+
+		// Set a variable to check if any errors are made in password
+		$validPassword = true;
+
+		if (strlen($valueTrim) !== $valueLength)
+		{
+			return false;
+		}
+
+		// Minimum number of integers required
+		if (!empty($minimumIntegers))
+		{
+			$nInts = preg_match_all('/[0-9]/', $value, $imatch);
+
+			if ($nInts < $minimumIntegers)
+			{
+				return false;
+			}
+		}
+
+		// Minimum number of symbols required
+		if (!empty($minimumSymbols))
+		{
+			$nsymbols = preg_match_all('[\W]', $value, $smatch);
+
+			if ($nsymbols < $minimumSymbols)
+			{
+				return false;
+			}
+		}
+
+		// Minimum number of upper case ASCII characters required
+		if (!empty($minimumUppercase))
+		{
+			$nUppercase = preg_match_all('/[A-Z]/', $value, $umatch);
+
+			if ($nUppercase < $minimumUppercase)
+			{
+				return false;
+			}
+		}
+
+		// Minimum length option
+		if (!empty($minimumLength))
 		{
 			return false;
 		}
