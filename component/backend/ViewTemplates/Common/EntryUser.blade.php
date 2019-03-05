90764e3fc01350dcae088873ffc610cb3c22aa41
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-03-05 18:59:04 +0200

    Joomla 4 compatibility of the user selection common template

--- component/backend/ViewTemplates/Common/EntryUser.blade.php
+++ component/backend/ViewTemplates/Common/EntryUser.blade.php
@@ -30,29 +30,51 @@ $userID      = $item->getFieldValue($field, 0);
 $user        = $item->getContainer()->platform->getUser($userID);
 $width       = isset($width) ? $width : 800;
 $height      = isset($height) ? $height : 500;
+$class       = isset($class) ? $class : '';
+$size        = isset($size) ? $size : 0;
+$onchange        = isset($onchange) ? $onchange : '';
 
 $uri = new JUri('index.php?option=com_users&view=users&layout=modal&tmpl=component');
 $uri->setVar('required', (isset($required) ? ($required ? 1 : 0) : 0));
 $uri->setVar('field', $field);
 $url = 'index.php' . $uri->toString(['query']);
 ?>
-@unless($readonly)
-@jhtml('behavior.modal', 'a.userSelectModal_' . $this->escape($field))
-@jhtml('script', 'jui/fielduser.min.js', ['version' => 'auto', 'relative' => true])
-@endunless
+@if (version_compare(JVERSION, '3.999999.999999', 'le'))
 
-<div class="akeeba-input-group">
-    <input readonly type="text"
-           id="{{{ $field }}}" value="{{{ $user->username }}}"
-           placeholder="{{{ $placeholder }}}"/>
-    <span class="akeeba-input-group-btn">
+	@unless($readonly)
+		@jhtml('behavior.modal', 'a.userSelectModal_' . $this->escape($field))
+		@jhtml('script', 'jui/fielduser.min.js', ['version' => 'auto', 'relative' => true])
+	@endunless
+
+	<div class="akeeba-input-group">
+		<input readonly type="text"
+			   id="{{{ $field }}}" value="{{{ $user->username }}}"
+			   placeholder="{{{ $placeholder }}}"/>
+		<span class="akeeba-input-group-btn">
 		<a href="@route($url)"
-           class="akeeba-btn--grey userSelectModal_{{{ $field }}}" title="{{{ $placeholder }}}"
-           rel="{handler: 'iframe', size: {x: {{$width}}, y: {{$height}} }}">
+		   class="akeeba-btn--grey userSelectModal_{{{ $field }}}" title="{{{ $placeholder }}}"
+		   rel="{handler: 'iframe', size: {x: {{$width}}, y: {{$height}} }}">
 			<span class="akion-person"></span>
 		</a>
 	</span>
-</div>
-@unless($readonly)
-<input type="hidden" id="{{{ $field }}}_id" name="{{{ $field }}}" value="{{ (int) $userID }}"/>
-@endunless
+	</div>
+	@unless($readonly)
+		<input type="hidden" id="{{{ $field }}}_id" name="{{{ $field }}}" value="{{ (int) $userID }}"/>
+	@endunless
+
+@else
+
+	@jlayout('joomla/form/field/user', [
+	'name' => $field,
+	'id' => $id,
+	'class' => $class,
+	'size' => $size,
+	'value' => $userID,
+	'userName' => $user->name,
+	'hint' => $placeholder,
+	'readonly' => $readonly,
+	'required' => $required,
+	'onchange' => $onchange,
+	])
+
+@endif
