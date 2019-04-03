155e875db11bfa70a909a4d1112536145f0e6afd
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-04-03 11:08:52 +0300

    The layout was wacky when not logged in
    gh-332

--- component/frontend/View/Level/tmpl/default_account.blade.php
+++ component/frontend/View/Level/tmpl/default_account.blade.php
@@ -51,113 +51,112 @@ $returnURI->setVar('reset', 1);
 		<h3>
 			@lang('COM_AKEEBASUBS_LEVEL_LBL_ACCOUNTHEADER')
 		</h3>
+	</header>
 
-		<div class="akeeba-form--horizontal akeebasubs-signup-fields">
 
-			<h3>@lang('COM_AKEEBASUBS_LEVEL_USERACCOUNT')</h3>
+	<div class="akeeba-form--horizontal akeebasubs-signup-fields">
 
-			{{-- Login button --}}
-			<div id="akeebasubs-level-login" class="akeeba-form-group--pull-right">
-				<div class="akeeba-form-group--actions">
-					<a href="@route('index.php?option=com_users&view=login&return=' . base64_encode($returnURI->toString())))"
-					   class="akeeba-btn--primary" rel="nofollow,noindex">
-						<span class="glyphicon glyphicon-log-in"></span>
-						@lang('COM_AKEEBASUBS_LEVEL_BTN_LOGINIFALERADY')
-					</a>
-				</div>
-			</div>
-
-			{{-- Full name --}}
-			<div class="akeeba-form-group{{{$group_classes['name']}}}">
-				<label for="name">
-					@lang('COM_AKEEBASUBS_LEVEL_FIELD_NAME')
-				</label>
-
-				<input type="text" name="name" id="name"
-					   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_NAME')"
-					   value="{{{$field_data['name']}}}"/>
-
-				<p id="name_empty" class="akeeba-help-text"
-				   <?php if (strpos($group_classes['name'], 'error') === false): ?>style="display:none"<?php endif ?>>
-					@lang('COM_AKEEBASUBS_LEVEL_ERR_NAME_INVALID')
-				</p>
-			</div>
-
-			{{-- Username --}}
-			<div class="akeeba-form-group{{$group_classes['username']}}">
-				<label for="username">
-					@lang('COM_AKEEBASUBS_LEVEL_FIELD_USERNAME')
-				</label>
-
-				<input type="text" name="username" id="username"
-					   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_USERNAME')"
-					   value="{{{$this->cache['username']}}}"/>
-
-				<p id="username_invalid" class="akeeba-help-text"
-				   <?php if (strpos($group_classes['username'], 'error') === false): ?>style="display:none"<?php endif ?>>
-					@lang('COM_AKEEBASUBS_LEVEL_FIELD_USERNAME_INVALID')
-				</p>
-			</div>
-
-			{{-- Password --}}
-			<div class="akeeba-form-group{{$group_classes['password']}}">
-				<label for="password">
-					@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD')
-				</label>
-				<label for="password2" aria-hidden="false" style="display: none;">
-					@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD2')
-				</label>
-
-				<div class="akeeba-form-group--radio">
-					<input type="password" name="password" id="password"
-						   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD')"
-						   value="{{{$this->cache['password']}}}"/>
-					<input type="password" name="password2" id="password2"
-						   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD2')"
-						   value="{{{$this->cache['password2']}}}"/>
-				</div>
-
-				<p id="password_invalid" class="akeeba-help-text"
-				   style="<?php if (strpos($group_classes['password'], 'error') === false): ?>display:none<?php endif; ?>">
-					@lang('COM_AKEEBASUBS_LEVEL_ERR_PASSWORD_EMPTY')
-				</p>
-				<p id="password2_invalid" class="help-block"
-				   style="<?php if (strpos($group_classes['password2'], 'error') === false): ?>display:none<?php endif; ?>">
-					@lang('COM_AKEEBASUBS_LEVEL_ERR_PASSWORD2')
-				</p>
-			</div>
-
-			{{-- Email --}}
-			<div class="akeeba-form-group{{$group_classes['email']}}">
-				<label for="email">
-					@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL')
-				</label>
-				<label for="email2" aria-hidden="false" style="display: none;">
-					@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL2')
-				</label>
-
-				<div class="akeeba-form-group--radio">
-					<input type="text" name="email" id="email"
-						   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL')"
-						   value="{{{$field_data['email']}}}"/>
-
-					<input type="text" name="email2" id="email2"
-						   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL2')"
-						   value="{{{$field_data['email2']}}}"/>
-				</div>
-
-				<p id="email_invalid" class="akeeba-help-text"
-				   <?php if (strpos($group_classes['email'], 'error') === false): ?>style="display:none"<?php endif ?>>
-					@lang('COM_AKEEBASUBS_LEVEL_ERR_EMAIL')
-				</p>
-
-				<p id="email2_invalid" class="akeeba-help-text"
-				   <?php if (strpos($group_classes['email2'], 'error') === false): ?>style="display:none"<?php endif ?>>
-					@lang('COM_AKEEBASUBS_LEVEL_ERR_EMAIL2')
-				</p>
+		{{-- Login button --}}
+		<div id="akeebasubs-level-login" class="akeeba-form-group--pull-right">
+			<div class="akeeba-form-group--actions">
+				<a href="@route('index.php?option=com_users&view=login&return=' . base64_encode($returnURI->toString())))"
+				   class="akeeba-btn--primary" rel="nofollow,noindex">
+					<span class="glyphicon glyphicon-log-in"></span>
+					@lang('COM_AKEEBASUBS_LEVEL_BTN_LOGINIFALERADY')
+				</a>
 			</div>
 		</div>
-	</header>
+
+		{{-- Full name --}}
+		<div class="akeeba-form-group{{{$group_classes['name']}}}">
+			<label for="name">
+				@lang('COM_AKEEBASUBS_LEVEL_FIELD_NAME')
+			</label>
+
+			<input type="text" name="name" id="name"
+				   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_NAME')"
+				   value="{{{$field_data['name']}}}"/>
+
+			<p id="name_empty" class="akeeba-help-text"
+			   <?php if (strpos($group_classes['name'], 'error') === false): ?>style="display:none"<?php endif ?>>
+				@lang('COM_AKEEBASUBS_LEVEL_ERR_NAME_INVALID')
+			</p>
+		</div>
+
+		{{-- Username --}}
+		<div class="akeeba-form-group{{$group_classes['username']}}">
+			<label for="username">
+				@lang('COM_AKEEBASUBS_LEVEL_FIELD_USERNAME')
+			</label>
+
+			<input type="text" name="username" id="username"
+				   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_USERNAME')"
+				   value="{{{$this->cache['username']}}}"/>
+
+			<p id="username_invalid" class="akeeba-help-text"
+			   <?php if (strpos($group_classes['username'], 'error') === false): ?>style="display:none"<?php endif ?>>
+				@lang('COM_AKEEBASUBS_LEVEL_FIELD_USERNAME_INVALID')
+			</p>
+		</div>
+
+		{{-- Password --}}
+		<div class="akeeba-form-group{{$group_classes['password']}}">
+			<label for="password">
+				@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD')
+			</label>
+			<label for="password2" aria-hidden="false" style="display: none;">
+				@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD2')
+			</label>
+
+			<div class="akeeba-form-group--radio">
+				<input type="password" name="password" id="password"
+					   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD')"
+					   value="{{{$this->cache['password']}}}"/>
+				<input type="password" name="password2" id="password2"
+					   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_PASSWORD2')"
+					   value="{{{$this->cache['password2']}}}"/>
+			</div>
+
+			<p id="password_invalid" class="akeeba-help-text"
+			   style="<?php if (strpos($group_classes['password'], 'error') === false): ?>display:none<?php endif; ?>">
+				@lang('COM_AKEEBASUBS_LEVEL_ERR_PASSWORD_EMPTY')
+			</p>
+			<p id="password2_invalid" class="help-block"
+			   style="<?php if (strpos($group_classes['password2'], 'error') === false): ?>display:none<?php endif; ?>">
+				@lang('COM_AKEEBASUBS_LEVEL_ERR_PASSWORD2')
+			</p>
+		</div>
+
+		{{-- Email --}}
+		<div class="akeeba-form-group{{$group_classes['email']}}">
+			<label for="email">
+				@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL')
+			</label>
+			<label for="email2" aria-hidden="false" style="display: none;">
+				@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL2')
+			</label>
+
+			<div class="akeeba-form-group--radio">
+				<input type="text" name="email" id="email"
+					   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL')"
+					   value="{{{$field_data['email']}}}"/>
+
+				<input type="text" name="email2" id="email2"
+					   placeholder="@lang('COM_AKEEBASUBS_LEVEL_FIELD_EMAIL2')"
+					   value="{{{$field_data['email2']}}}"/>
+			</div>
+
+			<p id="email_invalid" class="akeeba-help-text"
+			   <?php if (strpos($group_classes['email'], 'error') === false): ?>style="display:none"<?php endif ?>>
+				@lang('COM_AKEEBASUBS_LEVEL_ERR_EMAIL')
+			</p>
+
+			<p id="email2_invalid" class="akeeba-help-text"
+			   <?php if (strpos($group_classes['email2'], 'error') === false): ?>style="display:none"<?php endif ?>>
+				@lang('COM_AKEEBASUBS_LEVEL_ERR_EMAIL2')
+			</p>
+		</div>
+	</div>
 </div>
 @endif
 
