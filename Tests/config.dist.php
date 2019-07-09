e96f6db0c50527ca559a71ca5f7110c2091689fe
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-07-09 10:05:16 +0300

    Rewrite all the unit tests
    Remove tax setup, apply schema changes on tests bootstrap

--- Tests/config.dist.php
+++ Tests/config.dist.php
@@ -2,7 +2,7 @@
 /**
  * @package    AkeebaSubs
  * @subpackage Tests
- * @copyright Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @copyright  Copyright (c)2010-2019 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license    GNU General Public License version 3, or later
  */
 
@@ -17,4 +17,25 @@ $akeebasubsTestConfig = [
 	'site_root' => '/var/www/guineapig',
 	'site_name' => 'Akeeba Subscriptions Unit Tests',
 	'site_url'  => 'http://localhost/guineapig/',
+	// Only used for determining the recurring price
+	'products'  => [
+		'single_product_a' => [
+			'paddle_product_id'  => '12345',
+			'paddle_secret'      => 'abcdef',
+			'paddle_plan_id'     => '12345',
+			'paddle_plan_secret' => 'abcdef',
+		],
+		'single_product_b' => [
+			'paddle_product_id'  => '12345',
+			'paddle_secret'      => 'abcdef',
+			'paddle_plan_id'     => '12345',
+			'paddle_plan_secret' => 'abcdef',
+		],
+		'bundle'           => [
+			'paddle_product_id'  => '12345',
+			'paddle_secret'      => 'abcdef',
+			'paddle_plan_id'     => '12345',
+			'paddle_plan_secret' => 'abcdef',
+		],
+	],
 ];
