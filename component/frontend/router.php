568391cb4f0c0736580c7cbd6ae5e57cc02a7141
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-05-03 00:12:56 +0300

    Joomla 4 beta 1 compatibility
    Router refactoring: build route
    
    I added a route preprocessing to add the Itemid before Joomla calls
    the SEF routing code. This applies even when SEF is disabled.

--- component/frontend/router.php
+++ component/frontend/router.php
@@ -8,6 +8,9 @@
 use FOF30\Container\Container;
 use Joomla\CMS\Component\Router\RouterBase;
 use Joomla\CMS\Factory;
+use Joomla\CMS\Menu\MenuItem;
+use Joomla\CMS\Uri\Uri;
+use Joomla\Registry\Registry;
 
 defined('_JEXEC') or die();
 
@@ -24,7 +27,112 @@ include_once JPATH_LIBRARIES . '/fof30/include.php';
  */
 class AkeebasubsRouter extends RouterBase
 {
+	private static $preprocessCache = [];
+	/**
+	 * The component container
+	 *
+	 * @var  Container|null
+	 */
 	private $container;
+
+	/**
+	 * Preprocess a URL query before building a SEF route.
+	 *
+	 * This method is used to add a missing Itemid, set a language parameter etc.
+	 *
+	 * This method is executed on each URL, regardless of the SEF mode status.
+	 *
+	 * @param   array  $query  An associative array of URL arguments
+	 *
+	 * @return  array  The URL arguments to use to assemble the subsequent URL.
+	 *
+	 * @since   7.1.1
+	 */
+	public function preprocess($query)
+	{
+		// Have I already processed this query before?
+		ksort($query);
+
+		$signature = md5(serialize($query));
+
+		if (isset(self::$preprocessCache[$signature]))
+		{
+			return self::$preprocessCache[$signature];
+		}
+
+		// Do I have an Itemid in the query?
+		$itemId = $query['Itemid'] ?? null;
+
+		// If there's no Itemid we will try to get the active menu item's ID
+		if (empty($itemId))
+		{
+			$activeMenuItem = $this->menu->getActive();
+			$itemID         = (is_object($activeMenuItem) && property_exists($activeMenuItem, 'id')) ?
+				$activeMenuItem->id : null;
+		}
+
+		// Do I have a menu item ID at all?
+		$menuItem = null;
+
+		// If I have a menu item ID let's load the presumptive menu item.
+		if (!empty($itemID))
+		{
+			$menuItem = $this->menu->getItem($itemID);
+		}
+
+		// Check that the component matches
+		if (!empty($menuItem))
+		{
+			if ($menuItem->component != $this->getContainer()->componentName)
+			{
+				$menuItem = null;
+			}
+		}
+
+		// Check that the view matches our expectations
+		if (!empty($menuItem))
+		{
+			$uri  = Uri::getInstance($menuItem->link);
+			$view = $uri->getVar('view', 'Levels');
+
+			if (!in_array(strtolower($view), ['level', 'levels', 'new', 'subscribe']))
+			{
+				$menuItem = null;
+			}
+		}
+
+		// Otherwise, find a suitable menu -- exact match
+		if (empty($menuItem))
+		{
+			$menuItem = $this->findMenu(array_merge($query, [
+				'view' => $query['view'] ?? 'Levels',
+			]));
+		}
+
+		// Otherwise, find a suitable menu -- root node
+		if (empty($menuItem))
+		{
+			$menuItem = $this->findMenu([
+				'view' => $query['view'] ?? 'Levels',
+			]);
+		}
+
+		// We have an Itemid. Pass it along.
+		if (!empty($menuItem))
+		{
+			$query['Itemid'] = $menuItem->id;
+		}
+		// I really don't have a valid Itemid. Let's kill it with fire from our $query.
+		elseif (isset($query['Itemid']))
+		{
+			unset($query['Itemid']);
+		}
+
+		// Cache and return the result
+		self::$preprocessCache[$signature] = $query;
+
+		return $query;
+	}
 
 	/**
 	 * Build method for URLs
@@ -43,25 +151,18 @@ class AkeebasubsRouter extends RouterBase
 		$segments = [];
 
 		// Default view
-		$default = 'Levels';
+		$default   = 'Levels';
+		$menuquery = [];
 
 		// We need to find out if the menu item link has a view param
 		if (array_key_exists('Itemid', $query))
 		{
 			$menu = Factory::getApplication()->getMenu()->getItem($query['Itemid']);
 
-			if (!is_object($menu))
-			{
-				$menuquery = [];
-			}
-			else
+			if (is_object($menu))
 			{
 				parse_str(str_replace('index.php?', '', $menu->link), $menuquery); // remove "index.php?" and parse
 			}
-		}
-		else
-		{
-			$menuquery = [];
 		}
 
 		// Add the view
@@ -252,7 +353,7 @@ class AkeebasubsRouter extends RouterBase
 		}
 
 		// I need to remove all leftover segments in Joomla 4 to avoid a 404 error.
-        $segments = [];
+		$segments = [];
 
 		return $vars;
 	}
@@ -269,10 +370,171 @@ class AkeebasubsRouter extends RouterBase
 		if (is_null($this->container))
 		{
 			$this->container = Container::getInstance('com_akeebasubs', [
-				'tempInstance' => true
+				'tempInstance' => true,
 			]);
 		}
 
 		return $this->container;
+	}
+
+	/**
+	 * Finds a menu whose query parameters match those in $queryOptions
+	 *
+	 * @param   array  $queryOptions  The query parameters to look for
+	 * @param   array  $params        The menu parameters to look for
+	 *
+	 * @return  null|MenuItem  Null if not found, or the menu item if we did find it
+	 */
+	private function findMenu(array $queryOptions = [], array $params = []): ?MenuItem
+	{
+		$menuitem = $this->menu->getActive();
+
+		// First check the current menu item (fastest shortcut!)
+		if (is_object($menuitem))
+		{
+			if (self::checkMenu($menuitem, $queryOptions, $params))
+			{
+				return $menuitem;
+			}
+		}
+
+		foreach ($this->menu->getMenu() as $item)
+		{
+			if (self::checkMenu($item, $queryOptions, $params))
+			{
+				return $item;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Checks if a menu item conforms to the query options and parameters specified
+	 *
+	 * @param   MenuItem  $menu          A menu item
+	 * @param   array     $queryOptions  The query options to look for
+	 * @param   array     $params        The menu parameters to look for
+	 *
+	 * @return  bool
+	 */
+	private function checkMenu(MenuItem $menu, array $queryOptions, array $params = []): bool
+	{
+		static $languages = [];
+
+		if (empty($languages))
+		{
+			$languages = $this->getApplicationLanguages();
+		}
+
+		if (isset($queryOptions['lang']))
+		{
+			if (!empty($queryOptions['lang']))
+			{
+				$languages = [$queryOptions['lang']];
+			}
+
+			unset($queryOptions['lang']);
+		}
+
+		if (!empty($menu->language))
+		{
+			if (!in_array($menu->language, $languages))
+			{
+				return false;
+			}
+		}
+
+		$query = $menu->query;
+
+		foreach ($queryOptions as $key => $value)
+		{
+			// A null value was requested. Huh.
+			if (is_null($value))
+			{
+				// If the key is set and is not empty it's not the menu item you're looking for
+				if (isset($query[$key]) && !empty($query[$key]))
+				{
+					return false;
+				}
+
+				continue;
+			}
+
+			if (!isset($query[$key]))
+			{
+				return false;
+			}
+
+			if ($key == 'view')
+			{
+				// Treat views case-insensitive
+				if (strtolower($query[$key]) != strtolower($value))
+				{
+					return false;
+				}
+			}
+			elseif ($query[$key] != $value)
+			{
+				return false;
+			}
+		}
+
+		if (empty($params))
+		{
+			return true;
+		}
+
+		$menuItemParams = $menu->getParams();
+		$check          = $menuItemParams instanceof Registry ? $menuItemParams : $this->menu->getParams($menu->id);
+
+		foreach ($params as $key => $value)
+		{
+			if (is_null($value))
+			{
+				continue;
+			}
+
+			if ($check->get($key) != $value)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Returns the application's languages, in the preferred order for the current user.
+	 *
+	 * @return array|string[]
+	 *
+	 * @since  7.1.1
+	 */
+	private function getApplicationLanguages(): array
+	{
+		$container      = $this->getContainer();
+		$languages      = ['*'];
+		$isMultilingual = true;
+
+		if (!$container->platform->isCli() && !$container->platform->isBackend())
+		{
+			$isMultilingual = method_exists($this->app, 'getLanguageFilter') ?
+				$this->app->getLanguageFilter() : false;
+		}
+
+		if (!$isMultilingual)
+		{
+			return $languages;
+		}
+
+		// Get default site language
+		$jLang = $this->app->getLanguage();
+
+		return array_unique([
+			$jLang->getTag(),
+			$jLang->getDefault(),
+			'*',
+		]);
 	}
 }
