de4fc8b818a61730f8e4aabff06bcf8ef6855338
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-05-13 00:05:22 +0300

    Trying to solve the unsolvable problem
    It's be nice if Paddle actually communicated whether
    the client closed the dialog without paying, if they
    declined payment or if the payment is in progress. Since
    they don't we have to guide the client through a
    minefield on known unknowns. You know?

--- component/frontend/View/Message/tmpl/new.blade.php
+++ component/frontend/View/Message/tmpl/new.blade.php
@@ -8,8 +8,12 @@
 // Protect from unauthorized access
 defined('_JEXEC') or die();
 
+use Joomla\CMS\Date\Date;
+
 /** @var \Akeeba\Subscriptions\Site\View\Message\Html $this */
 
+$jThen = new Date($this->subscription->created_on);
+$timeAgo = time() - $jThen->toUnix();
 ?>
 <div class="akeeba-panel--info">
 	<header class="akeeba-block-header">
@@ -20,6 +24,7 @@ defined('_JEXEC') or die();
 		</h3>
 	</header>
 
+	@if ($timeAgo <= 1200)
 	<div class="akeeba-block--warning large">
 		<h2>
 			@lang('COM_AKEEBASUBS_MESSAGE_NEW_LBL_DONTPAYAGAIN')
@@ -27,8 +32,28 @@ defined('_JEXEC') or die();
 		<p>
 			@lang('COM_AKEEBASUBS_MESSAGE_NEW_LBL_WAITAFEWMINUTES')
 		</p>
-
 	</div>
+	@else
+	@include('site:com_akeebasubs/Level/paddlejs')
+	<div class="akeeba-block--warning large">
+		<h2>
+			@lang('COM_AKEEBASUBS_MESSAGE_NEW_LBL_PROBABLYFAILED')
+		</h2>
+		<p>
+			<a class="akeeba-btn--green--big"
+			   href="javascript:Paddle.Checkout.open({override: '{{ $this->subscription->payment_url }}', successCallback: 'akeebasubsCheckoutComplete', closeCallback: 'akeebasubsCheckoutClosed', eventCallback: 'akeebasubsCheckoutEvent'});">
+				<span class="akion-card"></span>
+				@lang('COM_AKEEBASUBS_SUBSCRIPTIONS_BTN_RETRYPAYMENT')
+			</a>
+
+			<a class="akeeba-btn--red--small"
+			   href="@route('index.php?option=com_akeebasubs&view=Subscribe&task=cancel_unpaid&id=' . $this->subscription->getId())">
+				<span class="akion-android-cancel"></span>
+				@lang('COM_AKEEBASUBS_SUBSCRIPTIONS_BTN_STARTOVER')
+			</a>
+		</p>
+	</div>
+	@endif
 
 	<p>
 		@lang('COM_AKEEBASUBS_MESSAGE_NEW_LBL_INFOTEXT1')
@@ -43,7 +68,7 @@ defined('_JEXEC') or die();
 	</p>
 
 	<p>
-		@lang('COM_AKEEBASUBS_MESSAGE_NEW_LBL_CANCELTEXT')
+		@sprintf('COM_AKEEBASUBS_MESSAGE_NEW_LBL_CANCELTEXT', \Joomla\CMS\Router\Route::_('index.php?option=com_akeebasubs&view=Subscribe&task=cancel_unpaid&id=' . $this->subscription->getId()))
 	</p>
 
 	<hr/>
